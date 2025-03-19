319aed58c420ce8d4a63b2db3d35c4db3d99aacf
valtsu23 <valtteri.eklund@gmail.com>
2025-03-19 03:18:17 +0200

    Update code_without_rtc.py

--- code_without_rtc.py
+++ code_without_rtc.py
@@ -1,4 +1,5 @@
-# Version 2.0
+# Version 3.0
+# Circuitpython 9 support
 # Added SD card check, error handling and SD card error message
 # Other small improvements
 
@@ -34,6 +35,10 @@ except OSError:
 if sd_card_ok == 1:
     vfs = storage.VfsFat(sdcard)
     storage.mount(vfs, "/sd")
+
+# RTC communication
+myI2C = busio.I2C(board.SCL, board.SDA)
+rtc = adafruit_ds3231.DS3231(myI2C)
 
 if False:   # change to True if you want to write the time!
     #                     year, mon, date, hour, min, sec, wday, yday, isdst
@@ -73,6 +78,7 @@ lines_group = displayio.Group(x=0, y=0)
 
 # Display group
 view = displayio.Group()
+display.root_group = view
 view.append(error_group)
 view.append(lines_group)
 view.append(tab_number_group)
@@ -120,15 +126,10 @@ tab_number_group.append(tab_number_label)
 
 # Data rows
 # Item 1
-# Coolant temp
-cltt_label = label.Label(fontS, text="CLT", color=data_color, scale=1)
-cltt_label.x = TABS_X + 10
-cltt_label.y = TABS_Y + 50
-
-# Lambda label objects
-clt_label = label.Label(fontB, text="NA", color=data_color, scale=1)
-clt_label.x = TABS_X + 105
-clt_label.y = TABS_Y + 50
+# Clock label objects
+clock_label = label.Label(fontB, text="00:00", color=data_color, scale=1)
+clock_label.x = TABS_X + 38
+clock_label.y = TABS_Y + 50
 
 # Item 2
 # Lambda text label objects
@@ -164,17 +165,6 @@ oilt_label = label.Label(fontB, text="NA", color=data_color, scale=1)
 oilt_label.x = TABS_X + 105
 oilt_label.y = TABS_Y + 50
 
-# Item 5
-# Intake air temperature text label objects
-iatt_label = label.Label(fontS, text="IAT", color=data_color, scale=1)
-iatt_label.x = TABS_X + 10
-iatt_label.y = TABS_Y + 50
-
-# Oil temperature label objects
-iat_label = label.Label(fontB, text="NA", color=data_color, scale=1)
-iat_label.x = TABS_X + 105
-iat_label.y = TABS_Y + 50
-
 # Rows
 row1 = row1_group
 row2 = row2_group
@@ -188,10 +178,9 @@ def tab1():
     clear_row(row1)
     clear_row(row2)
     clear_row(row3)
-    row1_group.append(lambdat_label)
-    row1_group.append(lambda_label)
-    row2_group.append(cltt_label)
-    row2_group.append(clt_label)
+    row1_group.append(clock_label)
+    row2_group.append(lambdat_label)
+    row2_group.append(lambda_label)
     row3_group.append(oiltt_label)
     row3_group.append(oilt_label)
 
@@ -199,10 +188,9 @@ def tab2():
     clear_row(row1)
     clear_row(row2)
     clear_row(row3)
-    row1_group.append(lambdat_label)
-    row1_group.append(lambda_label)
-    row2_group.append(oiltt_label)
-    row2_group.append(oilt_label)
+    row1_group.append(clock_label)
+    row2_group.append(lambdat_label)
+    row2_group.append(lambda_label)
     row3_group.append(oilpt_label)
     row3_group.append(oilp_label)
 
@@ -210,12 +198,12 @@ def tab3():
     clear_row(row1)
     clear_row(row2)
     clear_row(row3)
-    row1_group.append(lambdat_label)
-    row1_group.append(lambda_label)
-    row2_group.append(cltt_label)
-    row2_group.append(clt_label)
-    row3_group.append(iatt_label)
-    row3_group.append(iat_label)
+    row1_group.append(oiltt_label)
+    row1_group.append(oilt_label)
+    row2_group.append(lambdat_label)
+    row2_group.append(lambda_label)
+    row3_group.append(oilpt_label)
+    row3_group.append(oilp_label)
 
 # Read last tab drom sd card in startup if sd card ok
 if sd_card_ok == 1:
@@ -289,6 +277,10 @@ while True:
         touched = 0
         change_tab()
 
+    # Clock update
+    t = rtc.datetime
+    clock_label.text = "%02d:%02d" % (t.tm_hour, t.tm_min)
+
     # Update display
     display.root_group = view
 
@@ -313,21 +305,21 @@ while True:
         continue
 
     id = message.id
-    if id == 0x600:
-        # Unpack message
-        message = struct.unpack("<HBbHH", data)
-        # Just for debugging
-        # print(f"0x600: {message}")
-        # Update intake air temperature
-        iat_label.text = message[2]
     if id == 0x602:
+        # Unpack message
         message = struct.unpack("<HBBBBh", data)
-        oilt_label.text = message[2]
+        # Just for debugging
+        # print(f"0x602: {message}")
+        # Update oil pressure and oil temperature
+        oilt_label.text = str(message[2])
         oil_p = message[3]*0.0625
-        oilp_label.text = round(oil_p, 1)
-        clt_label.text = message[5]
+        oilp_label.text = str(round(oil_p, 1))
 
     if id == 0x603:
+        # Unpack message
         message = struct.unpack("<bBBBHH", data)
+        # Just for debugging
+        # print(f"0x603: {message}")
+        # Update lambda value
         lambda_value = message[2]*0.0078125
-        lambda_label.text = round(lambda_value, 2)
+        lambda_label.text = str(round(lambda_value, 2))
